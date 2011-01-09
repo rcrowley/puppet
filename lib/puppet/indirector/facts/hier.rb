d7ea8d0019e866e7467abc2c458da383b6c3b5c1
Richard Crowley <r@rcrowley.org>
2011-01-09 01:30:45 +0000

    Hierarchical facts terminus.

---
+++ lib/puppet/indirector/facts/hier.rb
@@ -0,0 +1,44 @@
+require 'puppet/indirector/facts/facter'
+
+class HierValue < Hash
+
+  attr_accessor :top
+
+  def initialize(top=nil)
+    @top = top
+  end
+
+  def to_s
+    @top
+  end
+
+end
+
+class Puppet::Node::Facts::Hier < Puppet::Node::Facts::Facter
+
+  def find(request)
+    hier = {}
+    make_hier = proc do |key, value|
+      value = value.split(",") if value.index(",")
+      h = hier
+      if key.index("_") and keys = key.split("_")
+        while 1 < keys.length and key = keys.shift
+          h = HierValue === h[key] ? h[key] : h[key] = HierValue.new(h[key])
+        end
+        key = keys.shift
+      end
+      if HierValue === h[key]
+        h[key].top = value
+      else
+        h[key] = value
+      end
+    end
+    super.values.reject { |k, v| Symbol == k.class }.each &make_hier
+    Puppet::Node::Facts.new(request.key, hier)
+  end
+
+  def destroy(facts); end
+
+  def save(facts); end
+
+end
